7f4f1149bb536ae1d214085185a0244f0b43505f
Orfik <orfiik@gmail.com>
2017-04-12 15:00:01 +0300

    travis test

---
+++ src/test/java/tests/testClass.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testClass {
+
+    @Test
+    public void forTsts(){
+        System.out.println("Test runs bad");
+    }
+
+    @Test
+    public void forTsts2(){
+        System.out.println("Test runs good");
+    }
+}
