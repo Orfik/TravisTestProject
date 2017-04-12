a74521fac25ecd97def06531bfc248f606726944
Orfik <orfiik@gmail.com>
2017-04-12 15:57:51 +0300

    travis test

--- src/test/java/tests/testClass.java
+++ src/test/java/tests/testClass.java
@@ -5,13 +5,8 @@ import org.junit.Test;
 
 public class testClass {
 
-    @Test
-    public void forTsts(){
-        System.out.println("Test runs bad");
+        @Test
+        public void writingToStdOut_succeeds() throws Exception {
+            System.out.println("I work!");
+        }
     }
-
-    @Test
-    public void forTsts2(){
-        System.out.println("Test runs good");
-    }
-}
