a71dbb878bcb65dd11fb99de8479161ab7503535
Orfik <orfiik@gmail.com>
2017-04-18 15:45:35 +0300

    travis test

--- src/test/java/tests/testClass.java
+++ src/test/java/tests/testClass.java
@@ -1,12 +1,26 @@
 package tests;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
 
 public class testClass {
 
-        @Test
-        public void writingToStdOut_succeeds() throws Exception {
-            System.out.println("I work!");
-        }
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver",  System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
     }
+}
