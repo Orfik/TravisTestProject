f128bef02d02737bae8f4204a32263b995140c76
Orfik <orfiik@gmail.com>
2017-04-19 14:07:52 +0300

    travis test

--- src/test/java/tests/testClass.java
+++ src/test/java/tests/testClass.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 
@@ -20,12 +22,18 @@ public class testClass {
         capabilities.setCapability("marionette", true);
         //driver = new RemoteWebDriver(capabilities);
         driver = new FirefoxDriver(capabilities);
-        driver.get("http://www.google.com/xhtml");
+        driver.get("https://stage.storied.co");
         Thread.sleep(5000);  // Let the user actually see something!
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("ChromeDriver");
-        searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement email = driver.findElement(By.id("login");
+        email.sendKeys("qa@storied.co");
+        WebElement password = driver.findElement(By.id("password");
+        password.sendKeys("zxc123");
+        WebElement button = driver.findElement(By.xpath(".//button[text()='Sign in']"));
+        button.click();
+        Thread.sleep(5000);
+        WebElement studioHome = driver.findElement(By.xpath("//*[@id='header']/div/div/a[1]"));
+        studioHome.click();
+        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id='current_account_id_chosen']/a/span"))); // Let the user actually see something!
         driver.quit();
         System.out.print("Work");
     }
