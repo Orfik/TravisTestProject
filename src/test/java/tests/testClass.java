9d51cb6343a378e74b98ce29fb14b19d7be655c9
Orfik <orfiik@gmail.com>
2017-04-19 10:41:36 +0300

    travis test

--- src/test/java/tests/testClass.java
+++ src/test/java/tests/testClass.java
@@ -11,15 +11,15 @@ import org.testng.annotations.Test;
 
 
 public class testClass {
-    protected RemoteWebDriver driver;
+    protected WebDriver driver;
 
     @Test
     public void testGoogleSearch() throws InterruptedException {
         System.setProperty("webdriver.gecko.driver",  System.getProperty("user.dir") + "//src//test/resources//geckodriver");
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
-        driver = new RemoteWebDriver(capabilities);
-        //driver = new FirefoxDriver();
+        //driver = new RemoteWebDriver(capabilities);
+        driver = new FirefoxDriver(capabilities);
         driver.get("http://www.google.com/xhtml");
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
